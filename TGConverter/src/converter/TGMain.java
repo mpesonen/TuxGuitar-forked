c5913a2c60c1d0a2024d32095a58bf8e8542dca0
Matti <matti.h.pesonen@gmail.com>
2017-01-21 01:14:47 +0200

    Referencing libs in pom.xml

--- TGConverter/src/converter/TGMain.java
+++ TGConverter/src/converter/TGMain.java
@@ -2,8 +2,8 @@ package converter;
 
 public class TGMain {
 
-	private static String SOURCE_FILE = "/full_path_to_folder/source.tg";
-	private static String TARGET_FILE = "/full_path_to_folder/target.mid";
+	private static String SOURCE_FILE = "/Users/matti/Desktop/guitar-tab-demo.gpx";
+	private static String TARGET_FILE = "/Users/matti/Desktop/guitar-tab-demo.gp5";
 	
 	public static void main(String[] s) {
 		TGFileConverter converter = new TGFileConverter();
